d8ccdbd66101417f35833167f382788cdaa49480
Skanda <skanda.td@gmail.com>
2025-04-23 14:40:36 +0530

    Springboot starters

---
+++ src/main/java/com/example/springbootessentials/SpringbootessentialsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootessentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootessentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootessentialsApplication.class, args);
+	}
+
+}
